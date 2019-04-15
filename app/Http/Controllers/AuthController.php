af3a8ce8b9dc0cc74cb55c10678df27e53e3f098
VukasinVujic <vukasinvujic021@gmail.com>
2019-04-15 20:56:04 +0200

    jwt, autentifikacija, seedovanje

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Controllers\Controller;
+use App\User;
+
+class AuthController extends Controller
+{
+
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api', ['except' => ['login']]);
+    // }
+
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+        $token = auth()->attempt($credentials);
+        if(!$token) {
+            return response()->json([
+                'message' => 'Wrong credentials'
+            ], 401);
+        }
+        return response()->json([
+            'token' => $token,
+            'type' => 'Bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+    }
+    public function register(RegisterRequest $request)
+    {
+        $user = new User();
+        $user->first_name = $request->input('first_name');
+        $user->last_name = $request->input('last_name');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+        return $this->login($request);
+    }
+}
